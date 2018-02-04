c98460f94cb5883baa6ac3cc8148aa1094a84dd0
sharp666 <36043022+sharp666@users.noreply.github.com>
2018-02-04 19:49:50 +0530

    first

---
+++ first.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n>0)
+{
+printf("positive number");
+}
+f(n<0)
+{
+printf("negative number");
+}
+f(n==0)
+{
+printf("zero");
+}
+
+}
